1f72a527d81becec15dcfc92b9790f9d21a85d42
MinhTuNA <viminhtu.101002@gmail.com>
2024-07-31 16:08:06 +0700

    add example

---
+++ example.py
@@ -0,0 +1,15 @@
+import DeltaX2Lib
+
+DeltaX2 = DeltaX2Lib.Deltax2Cmd()
+DeltaX2.Home()
+DeltaX2.SetAcceleration(1200)
+DeltaX2.SetSpeed(200)
+for x in range(5):
+    DeltaX2.MoveTo(Z=-300)
+    DeltaX2.MoveTo(X=0,Y=0)
+    DeltaX2.MoveTo(X=-100,Z=-350)
+    DeltaX2.MoveTo(Z=-300)
+    DeltaX2.MoveTo(X=0,Y=0)
+    DeltaX2.MoveTo(X=100,Z=-350)
+DeltaX2.Home()
+
