46515856a94cb777aec8e73c6cab9abc860aada3
MinhTuNA <viminhtu.101002@gmail.com>
2024-08-01 16:42:52 +0700

    update example

--- example.py
+++ example.py
@@ -12,4 +12,10 @@ for x in range(5):
     DeltaX2.MoveTo(X=0,Y=0)
     DeltaX2.MoveTo(X=100,Z=-350)
 DeltaX2.Home()
+DeltaX2.MoveTo(Z=-350)
+DeltaX2.MoveTo(X=50)
+DeltaX2.ArcMove(1,X=-50,Y=0,I=-50,J=0)
+DeltaX2.ArcMove(0,X=50,Y=0,I=50,J=0)
+
+
 
